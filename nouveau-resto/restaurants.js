5f610a024fc15fedc09b38b33edea68c9c1e767d
compush-media <tirakountech@gmail.com>
2025-12-20 02:44:44 +0100

    Add files via upload

---
+++ nouveau-resto/restaurants.js
@@ -0,0 +1,12 @@
+const restaurants = {
+  "nouveau-resto": {
+    name: "Nouveau Restaurant",
+    brandColor: "#C6A667",
+    bgColor: "#fff8f2",
+    textColor: "#C6A667",
+    iconColor: "#C6A667",
+    phone: "0102030405",
+    address: "Adresse du restaurant",
+    googleReview: "https://g.page/r/XXXX/review"
+  }
+};
